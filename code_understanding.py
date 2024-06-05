42ab1cba1fb0494b60a31097677490e46e0c2489
unknown <chenyih0111@gmail.com>
2024-06-05 10:55:57 +1000

    till extraction

--- code_understanding.py
+++ code_understanding.py
@@ -9,3 +9,22 @@ load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
+# Define the repository path on your local machine
+repo_path = "C:/Users/APOLLO/Desktop/test_repo"
+
+# URL of the remote repository
+repo_url = "https://github.com/langchain-ai/langchain"
+
+# Cloning the repository
+Repo.clone_from(repo_url, to_path=repo_path)
+
+# # Load
+# loader = GenericLoader.from_filesystem(
+#     repo_path + "/libs/core/langchain_core",
+#     glob="**/*",
+#     suffixes=[".py"],
+#     exclude=["**/non-utf8-encoding.py"],
+#     parser = LanguageParser(language=Language.PYTHON, parser_threshold=500),
+# )
+# documents = loader.load()
+# print(len(documents))
